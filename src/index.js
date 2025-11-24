547e09d29a21ba42ce24825a4191ace0e095180e
Sally <causeysally@gmail.com>
2025-11-23 21:11:44 -0600

    setup express and get/snacks

--- src/index.js
+++ src/index.js
@@ -4,22 +4,24 @@ import { randomUUID } from "node:crypto";
 const app = express();
 const PORT = 3000;
 
+// Middleware to parse JSON bodies
 app.use(express.json());
 
-app.use((req, res, next) => {
-  const bodyToLog = ["POST", "PUT", "PATCH"].includes(req.method) ? req.body : {};
-  console.log(`${req.method} ${req.path}`, { body: bodyToLog, query: req.query, params: req.params });
+// data array with seed data
+let snacksStorage = [
+  { id: randomUUID(), name: "Cheezit", price: 4.99, tags: ["crackers", "cheese snack"] },
+  { id: randomUUID(), name: "Pringles", price: 2.49, tags: ["potato", "flavored crisp"] },
+  { id: randomUUID(), name: "Cakes", price: 4.49, tags: ["Debbie cakes", "iced buns"] },
+];
 
-  res.on("finish", () => {
-    console.log(`Response status: ${res.statusCode}`);
-  });
-
-  next();
+// GET /snacks to return all snacks
+app.get("/snacks", (req, res) => {
+  res.status(200).json({ snacks: snacksStorage });
 });
 
+// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
 export default app;
- 
