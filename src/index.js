11d94bba521c3fe156cbdad29cd524a15c6091eb
Sally <causeysally@gmail.com>
2025-11-23 21:25:22 -0600

    added post itms

--- src/index.js
+++ src/index.js
@@ -12,12 +12,31 @@ let snacksStorage = [
   { id: randomUUID(), name: "Cheezit", price: 4.99, tags: ["crackers", "cheese snack"] },
   { id: randomUUID(), name: "Pringles", price: 2.49, tags: ["potato", "flavored crisp"] },
   { id: randomUUID(), name: "Cakes", price: 4.49, tags: ["Debbie cakes", "iced buns"] },
+  { id: randomUUID(), name: "Juice", price: 2.59, tags: ["Hawaiian Punch", "thirst quincher"] },
+  { id: randomUUID(), name: "PopTarts", price: 2.59, tags: ["chocolate", "breakfast snack"] },
+
 ];
 
 // GET /snacks to return all snacks
 app.get("/snacks", (req, res) => {
   res.status(200).json({ snacks: snacksStorage });
 });
+
+app.post("/snacks", (req, res) => {
+  const { name, price, tags } = req.body;
+
+  // Validation for post itms
+  if (!name) return res.status(400).json({ error: "Snack name is required" });
+  if (price === undefined || typeof price !== "number")
+    return res.status(400).json({ error: "Snack price is required and must be a number" });
+
+  // Creating new snacks
+  const newSnack = { id: randomUUID(), name, price, tags: Array.isArray(tags) ? tags : [] };
+  snacksStorage.push(newSnack);
+
+  res.status(201).json(newSnack);
+});
+
 
 // Start server
 app.listen(PORT, () => {
