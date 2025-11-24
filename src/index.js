c50e9384b41142257b60b4e62931d1a733e3bb73
Sally <causeysally@gmail.com>
2025-11-23 22:09:16 -0600

    checking functionality

--- src/index.js
+++ src/index.js
@@ -7,20 +7,38 @@ const PORT = 3000;
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-
-// 404 route error codin
-app.use((req, res) => {
-  res.status(404).json({ error: "Route not found" });
-});
-
 // data array with seed data
 let snacksStorage = [
-  { id: randomUUID(), name: "Cheezit", price: 4.99, tags: ["crackers", "cheese snack"] },
-  { id: randomUUID(), name: "Pringles", price: 2.49, tags: ["potato", "flavored crisp"] },
-  { id: randomUUID(), name: "Cakes", price: 4.49, tags: ["Debbie cakes", "iced buns"] },
-  { id: randomUUID(), name: "Juice", price: 2.59, tags: ["Hawaiian Punch", "thirst quincher"] },
-  { id: randomUUID(), name: "PopTarts", price: 2.59, tags: ["chocolate", "breakfast snack"] },
-
+  {
+    id: randomUUID(),
+    name: "Cheezit",
+    price: 4.99,
+    tags: ["crackers", "cheese snack"],
+  },
+  {
+    id: randomUUID(),
+    name: "Pringles",
+    price: 2.49,
+    tags: ["potato", "flavored crisp"],
+  },
+  {
+    id: randomUUID(),
+    name: "Cakes",
+    price: 4.49,
+    tags: ["Debbie cakes", "iced buns"],
+  },
+  {
+    id: randomUUID(),
+    name: "Juice",
+    price: 2.59,
+    tags: ["Hawaiian Punch", "thirst quincher"],
+  },
+  {
+    id: randomUUID(),
+    name: "PopTarts",
+    price: 2.59,
+    tags: ["chocolate", "breakfast snack"],
+  },
 ];
 
 // GET /snacks to return all snacks
@@ -34,10 +52,17 @@ app.post("/snacks", (req, res) => {
   // Validation for post itms
   if (!name) return res.status(400).json({ error: "Snack name is required" });
   if (price === undefined || typeof price !== "number")
-    return res.status(400).json({ error: "Snack price is required and must be a number" });
+    return res
+      .status(400)
+      .json({ error: "Snack price is required and must be a number" });
 
   // Creating new snacks
-  const newSnack = { id: randomUUID(), name, price, tags: Array.isArray(tags) ? tags : [] };
+  const newSnack = {
+    id: randomUUID(),
+    name,
+    price,
+    tags: Array.isArray(tags) ? tags : [],
+  };
   snacksStorage.push(newSnack);
 
   res.status(201).json(newSnack);
@@ -66,7 +91,10 @@ app.get("/", (req, res) => {
 app.post("/echo", (req, res) => {
   res.json({ received: req.body, message: "Echo successful!" });
 });
-
+// 404 route error codin
+app.use((req, res) => {
+  res.status(404).json({ error: "Route not found" });
+});
 // Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
