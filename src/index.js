639d57bf0f3b335f73b16d6efafb71d01ab56ec7
Sally <causeysally@gmail.com>
2025-11-23 21:07:38 -0600

    middleware

--- src/index.js
+++ src/index.js
@@ -6,6 +6,17 @@ const PORT = 3000;
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  const bodyToLog = ["POST", "PUT", "PATCH"].includes(req.method) ? req.body : {};
+  console.log(`${req.method} ${req.path}`, { body: bodyToLog, query: req.query, params: req.params });
+
+  res.on("finish", () => {
+    console.log(`Response status: ${res.statusCode}`);
+  });
+
+  next();
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
