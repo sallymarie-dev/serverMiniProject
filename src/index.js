228adbf11a2d308a14e1317e145f33c51fd75c72
Sally <causeysally@gmail.com>
2025-11-23 21:05:40 -0600

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import express from "express";
+import { randomUUID } from "node:crypto";
+
 const app = express();
 const PORT = 3000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+export default app;
+ 
